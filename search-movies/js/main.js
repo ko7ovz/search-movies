b19e8a2784608edf69e11d26a799b5d6525a99e5
ko7ovz <51297745+ko7ovz@users.noreply.github.com>
2019-06-20 06:17:50 -0700

    Update main.js

--- search-movies/js/main.js
+++ search-movies/js/main.js
@@ -2,24 +2,23 @@
 const searchForm = document.querySelector('#search-form'), movies = document.querySelector('#movies');
 function apiSearch(event) {
     event.preventDefault();
-    const searchText = document.querySelector('#searchText').value;
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=4eda6ad7231230beb4c59e4ca893b654&language=ru&query='
-        + searchText;
-    const url = 'https://image.tmdb.org/t/p/w500';
+    const searchText = document.querySelector('#searchText').value,
+          server = 'https://api.themoviedb.org/3/search/multi?api_key=4eda6ad7231230beb4c59e4ca893b654&language=ru&query='
+            + searchText,
+          url = 'https://image.tmdb.org/t/p/w500';
     movies.innerHTML = 'Loading..';
     fetch(server)
         .then(function (value) {
             return value.json();
         })
         .then(function (output) {
-            let inner = '', images = '', date = '', rating = '', overview = '';
+            let inner = '', images = '', rating = '';
             output.results.forEach(function (item) {
                 let nameItem = item.name || item.title, imgs = item.poster_path,
-                    dates = item.first_air_date || item.release_date, rate = item.vote_average, over = item.overview;
-                overview = over;
+                    date = item.first_air_date || item.release_date, rate = item.vote_average, overview = item.overview;
                 rating = `Rating: ${rate}`;
-                date = dates;
                 images = url + imgs ;
+                console.log(output.results);
                 if (item.poster_path === null) {
                     images = 'https://i.ibb.co/W36TD0c/glo.jpg';
                 }
@@ -48,10 +47,10 @@ function apiSearch(event) {
             });
             movies.innerHTML = inner;
         })
-    .catch (function (reason) {
-        movies.innerHTML = 'Oops..Something wrong';
-        console.log('error: ' + reason.status);
-    })
+        .catch (function (reason) {
+            movies.innerHTML = 'Oops..Something wrong';
+            console.log('error: ' + reason.status);
+        })
 }
 searchForm.addEventListener('submit', apiSearch);
 
